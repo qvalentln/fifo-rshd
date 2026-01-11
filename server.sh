4aa2eacecf469d56ad7512f6980a50c82d518aca
qvalentln <95167780+qvalentln@users.noreply.github.com>
2026-01-11 18:41:20 +0200

    Refactor server.sh for improved path handling
    Updated paths and added comments for clarity.

--- server.sh
+++ server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+#aceeasi treaba cu fisierele ca la client
 K=3
-CONFIG_FILE="$HOME/server_config.cfg"
-REPLY_DIR="$HOME/tmp"
+CONFIG_FILE="server_config.cfg"
+REPLY_DIR="tmp"
+#daca cfg-ul este gol, numele default va fi main_fifo
 FIFO_NAME=$(cat "$CONFIG_FILE" 2>/dev/null || echo "main_fifo")
-FIFO_PATH="$HOME/$FIFO_NAME"
+FIFO_PATH="$FIFO_NAME"
 
 #rutina de curatare
 cleanup() {
@@ -12,7 +13,7 @@ cleanup() {
 
     	#1. trimit un semnal de exit custom catre fiecare slave prin pipe-uri
         for ((i=1; i<=K; i++)); do
-            pipe_path="$HOME/slave_$i"
+            pipe_path="slave_$i"
             if [ -p "$pipe_path" ]; then
                 echo "EXIT_SIG" > "$pipe_path" &
             fi
@@ -28,7 +29,7 @@ cleanup() {
     
         # 4. sterg pipe-urile
         rm -f "$FIFO_PATH"
-        rm -f "$HOME"/slave_*
+        rm -f slave_*
     
         echo "exit&cleanup finalizat"
         exit 0
@@ -47,7 +48,7 @@ slid=()
 for ((i=1; i<=K; i++))
 do
     pipe_name="slave_$i"
-    pipe_path="$HOME/$pipe_name"
+    pipe_path="$pipe_name"
     [ -p "$pipe_path" ] || mkfifo "$pipe_path"
 
     # pornesc slave
@@ -71,13 +72,33 @@ do
             fi
         done" &
         
-    #retin PID
+    #retin PID, va fi util cand inchidem terminalele la 
+	#oprirea serverului
     slid+=($!)
 done
 icmd=0
 echo "master activ pe $FIFO_PATH"
 echo "CTRL+C pentru exit"
 
+
+#varianta mea cu round-robin (facuta pe branci, sa vedem daca e buna)
 while true; do
-	#exec cmd-uri mod n, urmeaza sa fie completat    
+	if read -r data <&3; then
+        [[ -z "$data" ]] && continue
+        
+        IFS=":" read -r CLIENT_PID COMMAND <<< "$data"
+        cmd=$(echo "$cmd" | xargs)
+
+		#reply_dir lowkey e doar tmp
+        fileName="$REPLY_DIR/server_reply-$CLIENT_PID"
+        
+        final_instruction="$cmd > \"$fileName\" 2>&1"
+
+        slave_idx=$(( (icmd % K) + 1 ))
+        target_pipe="slave_$slave_idx"
+
+        echo "Comanda de la $CLIENT_PID trimisa la slave-ul $slave_idx"
+        echo "$final_instruction" > "$target_pipe" &
+        ((icmd++))
+    fi
 done
